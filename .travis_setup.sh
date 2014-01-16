cdcdcd6011f394f9bf62fcea6e07bf0b119bf93a
Alexey Melnichuk <mimir@newmail.ru>
2014-01-16 14:06:43 +0400

    Use separate file to install Lua/LuaRocks
    Based on https://github.com/mpeterv/literal.

---
+++ .travis_setup.sh
@@ -0,0 +1,39 @@
+# A script for setting up environment for travis-ci testing.
+# Sets up Lua and Luarocks.
+# LUA must be "Lua 5.1", "Lua 5.2" or "LuaJIT 2.0".
+
+if [ "$LUA" == "luajit" ]; then
+  curl http://luajit.org/download/LuaJIT-2.0.2.tar.gz | tar xz
+  cd LuaJIT-2.0.2
+  make && sudo make install
+  cd ..;
+else
+  if [ "$LUA" == "lua5.1" ]; then
+    curl http://www.lua.org/ftp/lua-5.1.5.tar.gz | tar xz
+    cd lua-5.1.5;
+  elif [ "$LUA" == "lua5.2" ]; then
+    curl http://www.lua.org/ftp/lua-5.2.3.tar.gz | tar xz
+    cd lua-5.2.3;
+  fi
+  sudo make linux install
+  cd $TRAVIS_BUILD_DIR;
+fi
+
+if [ $LUAROCKS_INSTALL = "git" ]; then
+  git clone https://github.com/keplerproject/luarocks.git
+  cd luarocks
+  git checkout $LUAROCKS_GITTAG;
+else
+  curl http://luarocks.org/releases/$LUAROCKS_BASE.tar.gz | tar xz
+  cd $LUAROCKS_BASE;
+fi
+
+if [ "$LUA" == "luajit" ]; then
+  ./configure --lua-suffix=jit --with-lua-include=/usr/local/include/luajit-2.0;
+else
+  ./configure;
+fi
+
+make && sudo make install
+
+cd $TRAVIS_BUILD_DIR
