8db2913e579ed89eca188cfcf4ce26f23e698010
Alexey Melnichuk <mimir@newmail.ru>
2014-06-09 14:42:45 +0500

    Test with LuaJIT master (2.0/2.1)

--- .travis/setup_lua.sh
+++ .travis/setup_lua.sh
@@ -23,12 +23,30 @@ if [ -z "$PLATFORM" ]; then
   fi;
 fi
 
-if [ "$LUA" == "luajit" ]; then
-  curl http://luajit.org/download/$LUAJIT_BASE.tar.gz | tar xz
+
+if [ "$(expr substr $LUA 1 6)" == "luajit" ]; then
+
+  if [ "$LUA" == "luajit" ]; then
+    curl http://luajit.org/download/$LUAJIT_BASE.tar.gz | tar xz;
+  else
+    git clone http://luajit.org/git/luajit-2.0.git $LUAJIT_BASE;
+  fi
+
   cd $LUAJIT_BASE
+
+  if [ "$LUA" == "luajit2.1" ]; then
+    git checkout v2.1;
+  fi
+
   make && sudo make install
-  sudo ln -s /usr/local/bin/luajit /usr/local/bin/lua
-  cd $TRAVIS_BUILD_DIR;
+
+  if [ "$LUA" == "luajit2.1" ]; then
+    sudo ln -s /usr/local/bin/luajit-2.1.0-alpha /usr/local/bin/luajit
+    sudo ln -s /usr/local/bin/luajit /usr/local/bin/lua;
+  else
+    sudo ln -s /usr/local/bin/luajit /usr/local/bin/lua;
+  fi;
+
 else
   if [ "$LUA" == "lua5.1" ]; then
     curl http://www.lua.org/ftp/lua-5.1.5.tar.gz | tar xz
@@ -37,9 +55,10 @@ else
     curl http://www.lua.org/ftp/lua-5.2.3.tar.gz | tar xz
     cd lua-5.2.3;
   fi
-  sudo make $PLATFORM install
-  cd $TRAVIS_BUILD_DIR;
+  sudo make $PLATFORM install;
 fi
+
+cd $TRAVIS_BUILD_DIR;
 
 LUAROCKS_BASE=luarocks-$LUAROCKS
 curl http://luarocks.org/releases/$LUAROCKS_BASE.tar.gz | tar xz
@@ -47,6 +66,10 @@ cd $LUAROCKS_BASE;
 
 if [ "$LUA" == "luajit" ]; then
   ./configure --lua-suffix=jit --with-lua-include=/usr/local/include/luajit-2.0;
+elif [ "$LUA" == "luajit2.0" ]; then
+  ./configure --lua-suffix=jit --with-lua-include=/usr/local/include/luajit-2.0;
+elif [ "$LUA" == "luajit2.1" ]; then
+  ./configure --lua-suffix=jit --with-lua-include=/usr/local/include/luajit-2.1;
 else
   ./configure;
 fi
@@ -57,7 +80,7 @@ cd $TRAVIS_BUILD_DIR
 
 rm -rf $LUAROCKS_BASE
 
-if [ "$LUA" == "luajit" ]; then
+if [ "$(expr substr $LUA 1 6)" == "luajit" ]; then
   rm -rf $LUAJIT_BASE;
 elif [ "$LUA" == "lua5.1" ]; then
   rm -rf lua-5.1.5;
