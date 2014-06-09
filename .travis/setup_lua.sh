93b4c1c44b326567010ad8c53722e6cff3475632
Alexey Melnichuk <mimir@newmail.ru>
2014-06-09 08:53:16 +0400

    Change. Auto detect platform name. Change. Rename LUAROCKS_VER to LUAROCKS

--- .travis/setup_lua.sh
+++ .travis/setup_lua.sh
@@ -5,6 +5,34 @@
 # LUA must be "lua5.1", "lua5.2" or "luajit".
 # PLATFORM must be "linux" or "macosx".
 
+if [ -z "$PLATFORM" ]; then
+  PLATFORM=$TRAVIS_OS_NAME;
+fi
+
+if [ "$PLATFORM" == "osx" ]; then
+  PLATFORM="macosx";
+fi
+
+# http://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linuxluz-
+if [ -z "$PLATFORM" ]; then
+  if [ "$(uname)" == "Linux" ]; then
+    PLATFORM="linux";
+  else
+    PLATFORM="macosx";
+  fi;
+fi
+
+echo "==========================="
+echo " uname      : $(uname)     "
+echo " uname      : $(uname -s)  "
+echo " uname      : $(expr substr $(uname -s) 1 5)"
+echo " Travis     : $TRAVIS_OS_NAME"
+echo " Platform   : $PLATFORM    "
+echo " Lua        : $LUA         "
+echo " LuaRocks   : $LUAROCKS    "
+echo "==========================="
+echo
+
 if [ "$LUA" == "luajit" ]; then
   curl http://luajit.org/download/LuaJIT-2.0.2.tar.gz | tar xz
   cd LuaJIT-2.0.2
@@ -23,7 +51,7 @@ else
   cd $TRAVIS_BUILD_DIR;
 fi
 
-LUAROCKS_BASE=luarocks-$LUAROCKS_VER
+LUAROCKS_BASE=luarocks-$LUAROCKS
 curl http://luarocks.org/releases/$LUAROCKS_BASE.tar.gz | tar xz
 cd $LUAROCKS_BASE;
 
