6e7572cf89b82854333254213d998163d4f9c4bc
Alexey Melnichuk <mimir@newmail.ru>
2015-03-19 12:24:54 +0400

    Fix. Install Lua 5.2.4

--- .travis/setup_lua.sh
+++ .travis/setup_lua.sh
@@ -57,7 +57,7 @@ else
     cd lua-5.1.5;
   elif [ "$LUA" == "lua5.2" ]; then
     curl http://www.lua.org/ftp/lua-5.2.4.tar.gz | tar xz
-    cd lua-5.2.3;
+    cd lua-5.2.4;
   elif [ "$LUA" == "lua5.3" ]; then
     curl http://www.lua.org/ftp/lua-5.3.0.tar.gz | tar xz
     cd lua-5.3.0;
@@ -97,7 +97,7 @@ if [ "$LUAJIT" == "yes" ]; then
 elif [ "$LUA" == "lua5.1" ]; then
   rm -rf lua-5.1.5;
 elif [ "$LUA" == "lua5.2" ]; then
-  rm -rf lua-5.2.3;
+  rm -rf lua-5.2.4;
 elif [ "$LUA" == "lua5.3" ]; then
   rm -rf lua-5.3.0;
 fi
