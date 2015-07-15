db26e521a2a12ff20613d7fd5337d5bbfb14f9f8
Alexey Melnichuk <mimir@newmail.ru>
2015-07-15 13:34:42 +0400

    Add. Tests on Lua compat build mode.

--- test/test.lua
+++ test/test.lua
@@ -1,13 +1,17 @@
 pcall(require, "luacov")
 
+
 print("------------------------------------")
-print("Lua version: " .. (_G.jit and _G.jit.version or _G._VERSION))
+print("Lua version: " .. (jit and jit.version or _VERSION))
 print("------------------------------------")
 print("")
 
 local HAS_RUNNER = not not lunit
 local lunit = require "lunit"
 local TEST_CASE = lunit.TEST_CASE
+
+local LUA_VER = _VERSION
+local unpack, pow, bit32 = unpack, math.pow, bit32
 
 local _ENV = TEST_CASE"some_test_case"
 
@@ -25,6 +29,19 @@ function test_3()
   local foo = require "foo"
   assert_function(foo.test_false)
   assert_false(foo.test_false())
+end
+
+if LUA_VER == 'Lua 5.2' then
+function test_lua_52()
+  assert_nil(unpack)
+end
+end
+
+if LUA_VER == 'Lua 5.3' then
+function test_lua_53()
+  assert_nil(pow)
+  assert_nil(bit32)
+end
 end
 
 if not HAS_RUNNER then lunit.run() end
